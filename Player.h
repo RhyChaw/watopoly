b4669d699b4019c977f31e638454e40a0784025b
.... <v46jain@uwaterloo.ca>
2025-03-19 15:32:22 -0400

    players public function final

--- Player.h
+++ Player.h
@@ -1,15 +1,14 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
+#ifndef WATOPOLY_PLAYER_H
+#define WATOPOLY_PLAYER_H
 #include <string>
 #include <vector>
-#include <memory>
-#include "Cell.h"
+#include "GameBoard.h"
+#include <string>
 
 class Player {
 private:
     std::string name;  //name of the player
-    char Char; //symbol of the player
+    char symbol; //symbol of the player
     int index; //index on the board [40]
     int position_initial; // dice rolling position
     double cash; //money
@@ -22,12 +21,14 @@ private:
     int turnsInTimsLine; // number of rolls player has to miss before he gets his turn
     int doubles_counter; // measures the amount of double a person has rolled
     bool isBankrupt;
-    int monopoly[8]; //monopoly if owns a set of cards
+    int monopolySet[8]; //monopoly if owns a set of cards
     int cups; //get out of jail freecard
 
 public:
     Building *buildings[28];
-    Player(const std::string &name, char symbol);
+    Player(std::string name, char symbol, int index, int position_initial);
+    Player(std::string name, char symbol, int ownedCups, double cash, int index, bool isTimLine, int turnsInTimsLine, int position_initial);
+    ~Player();
 
     // Getters
     std::string getName() const;
@@ -37,27 +38,54 @@ public:
     int getAsset() const;
     int getOwnedResidences() const;
     int getOwnedGyms() const;
-    bool isInTimsLine() const;
     int getTurnsInTimsLine() const;
     int getOwnedCups() const;
+    bool isInTimsLine() const;
+    bool isBankrupt() const;
     std::vector<std::shared_ptr<Cell>> getProperties() const;
+    bool checkMonopolyImprove(Building *building);
+    Building *findBuilding(const std::string &buildingName);
+    void printAsset();
 
     // Setters & Modifiers
     void setPosition(int newPosition);
     void setCash(int amount);
-    void addCup();
-    void removeCup();
+    void setTurnsInTimsLine(int turns);
+    void setBankrupt(bool b);
+    void setCups(int n);
     void moveToTimsLine();
     void leaveTimsLine();
-    bool checkBankrupt() const;
-    
-    // New Additions
+    void changeAsset(int c);
+    void changePropertyCount(int residences, int gyms);
+
+    // Gameplay Functions
     void pay(int amount);
     void receive(int amount);
     void addProperty(std::shared_ptr<Cell> property);
     void removeProperty(std::shared_ptr<Cell> property);
     void declareBankruptcy();
+    bool checkBankrupt() const;
+
+    // Dice & Tims Line Management
     bool doubles();
+    void addCup();
+    void removeCup();
+
+    // Property Management
+    bool checkMonopoly(int block);
+    void buyBuilding(Building *building);
+    void sellBuilding(Building *building);
+    void mortgage(Building *building);
+    void unmortgage(Building *building);
+    void buyImprovement(Building *building);
+    void sellImprovement(Building *building);
+
+    // Trading
+    void trade(Player *partner, double money, Building *building);
+    void trade(Player *partner, Building *building1, Building *building2);
+    void trade(Player *partner, Building *building, double money);
 };
 
-#endif // PLAYER_H
+#endif //WATOPOLY_PLAYER_H
+
+    
